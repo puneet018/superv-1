1b7fa230bbd6d8d05a30f3b54fb885ea91572fbb
puneet018 <nikkyvishwa90@gmail.com>
2021-08-04 00:11:59 +0530

    'f'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../assets/vendor/bootstrap/css/bootstrap.min.css','./app.component.css']
 })
 export class AppComponent {
   title = 'supervision';
